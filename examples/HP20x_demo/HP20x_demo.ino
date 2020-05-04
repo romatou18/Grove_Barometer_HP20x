898787c757bd892818d918d80e15740d8135a916
romain <romain.s@flightcell.net>
2020-05-05 10:53:40 +1200

    ESP32 example

--- examples/HP20x_demo/HP20x_demo.ino
+++ examples/HP20x_demo/HP20x_demo.ino
@@ -1,9 +1,9 @@
 /*
- * Demo name   : HP20x_dev demo 
+ * Demo name   : HP20x_dev demo on ESP32
  * Usage       : I2C PRECISION BAROMETER AND ALTIMETER [HP206C hopeRF] 
- * Author      : Oliver Wang from Seeed Studio
- * Version     : V0.1
- * Change log  : Add kalman filter 2014/04/04
+ * Author      : Romain Sacchettini from original Arduino lib Oliver Wang from Seeed Studio
+ * Version     : V0.2-ESP32
+ * Change log  : Added example on ESP32 2020/05/05
 */
 
 #include <HP20x_dev.h>
@@ -13,10 +13,61 @@
 unsigned char ret = 0;
 
 /* Instance */
-KalmanFilter t_filter;    //temperature filter
-KalmanFilter p_filter;    //pressure filter
-KalmanFilter a_filter;    //altitude filter
+grove::KalmanFilter t_filter;    //temperature filter
+grove::KalmanFilter p_filter;    //pressure filter
+grove::KalmanFilter a_filter;    //altitude filter
 
+
+#define I2C_CTRL 1
+#if I2C_CTRL == 0
+#define WireInstance Wire
+#define SDA 21
+#define SCL 22
+#define FREQ 400000L
+
+#else 
+#define WireInstance Wire1
+#define SDA 27
+#define SCL 26
+#define FREQ 400000L
+
+#endif
+HP20x_dev HP20x(I2C_CTRL, SDA, SCL, FREQ);
+#define ENABLE_I2C_DEBUG_BUFFER 1
+
+void scanner()
+{
+	 byte error, address;
+  int nDevices;
+  Serial.println("Scanning...");
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    WireInstance.beginTransmission(address);
+    error = WireInstance.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+      nDevices++;
+    }
+    else if (error==4) {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  }
+  else {
+    Serial.println("done\n");
+  }
+  delay(2000);  
+}
 
 void setup()
 {  
@@ -24,45 +75,68 @@ void setup()
   
   Serial.println("****HP20x_dev demo by seeed studio****\n");
   Serial.println("Calculation formula: H = [8.5(101325-P)]/100 \n");
+  WireInstance.begin(SDA, SCL, FREQ);
+  scanner();
   /* Power up,delay 150ms,until voltage is stable */
   delay(150);
   /* Reset HP20x_dev */
   HP20x.begin();
-  delay(100);
+  delay(1000);
+  
+  /* Determine HP20x_dev is available or not */
+  bool found = false;
+  while(!found)
+  {
+	ret = HP20x.isAvailable();
+	if(OK_HP20X_DEV == ret)
+	{
+		Serial.println("HP20x_dev is available.\n");   
+		found = true; 
+	}
+	else
+	{
+		Serial.println("HP20x_dev isn't available.\n");
+		delay(500);
+		ret = HP20x.isAvailable();
+	}
+  }
+  
 }
  
 
 void loop()
 {
-	Serial.println("------------------\n");
-	long Temper = HP20x.ReadTemperature();
-	Serial.println("Temper:");
-	float t = Temper/100.0;
-	Serial.print(t);	  
-	Serial.println("C.\n");
-	Serial.println("Filter:");
-	Serial.print(t_filter.Filter(t));
-	Serial.println("C.\n");
-
-	long Pressure = HP20x.ReadPressure();
-	Serial.println("Pressure:");
-	t = Pressure/100.0;
-	Serial.print(t);
-	Serial.println("hPa.\n");
-	Serial.println("Filter:");
-	Serial.print(p_filter.Filter(t));
-	Serial.println("hPa\n");
-	
-	long Altitude = HP20x.ReadAltitude();
-	Serial.println("Altitude:");
-	t = Altitude/100.0;
-	Serial.print(t);
-	Serial.println("m.\n");
-	Serial.println("Filter:");
-	Serial.print(a_filter.Filter(t));
-	Serial.println("m.\n");
-	Serial.println("------------------\n");
-	
-	delay(1000);
-}
+    char display[40];
+    if(OK_HP20X_DEV == ret)
+    { 
+	  Serial.println("------------------\n");
+	  long Temper = HP20x.ReadTemperature();
+	  Serial.println("Temper:");
+	  float t = Temper/100.0;
+	  Serial.print(t);	  
+	  Serial.println("C.\n");
+	  Serial.println("Filter:");
+	  Serial.print(t_filter.Filter(t));
+	  Serial.println("C.\n");
  
+      long Pressure = HP20x.ReadPressure();
+	  Serial.println("Pressure:");
+	  float p = Pressure/100.0;
+	  Serial.print(p);
+	  Serial.println("hPa.\n");
+	  Serial.println("Filter:");
+	  Serial.print(p_filter.Filter(p));
+	  Serial.println("hPa\n");
+	  
+	  long Altitude = HP20x.ReadAltitude();
+	  Serial.println("Altitude:");
+	  float a = Altitude/100.0;
+	  Serial.print(a);
+	  Serial.println("m.\n");
+	  Serial.println("Filter:");
+	  Serial.print(a_filter.Filter(a));
+	  Serial.println("m.\n");
+	  Serial.println("------------------\n");
+      delay(1000);
+    }
+}
